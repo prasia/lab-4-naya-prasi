3fe79d65cfa522c62d7e17c4a94e520e0e5f1c89
prasia <prasi040707@gmail.com>
2025-11-05 13:58:32 -0800

    data definitions

--- bst.py
+++ bst.py
@@ -3,3 +3,15 @@ import unittest
 from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6) 
+
+BinTree: TypeAlias = Union["Node", None]
+@dataclass
+class Node:
+    element: Any
+    value: Any
+
+@dataclass(frozen=True)
+class BinarySearchTree:
+    comes_before: Callable[[Any,Any],bool]
+    bt: BinTree
+
